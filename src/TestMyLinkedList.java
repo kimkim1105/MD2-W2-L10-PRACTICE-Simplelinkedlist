c2643445ddc7bd5402559563d075831cc7990f59
unknown <huenk105@gmail.com>
2022-04-13 22:30:13 +0700

    demo

---
+++ src/TestMyLinkedList.java
@@ -0,0 +1,14 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(2,9);
+        ll.add(5,7);
+        ll.add(6,9);
+        ll.printList();
+    }
+}
